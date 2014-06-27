458365a7ac804ad55348cc3090a70d4a7faaa37b
www-data <www-data@phlu-eduweb2.nine.ch>
2014-06-27 22:45:52 +0200

    Correctly comment on hook (refs #880)

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,5 +3,5 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-//
+// Implement the actions hook in the RecordList to manipulate the available icons in the List module
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['actions']['newspermissions'] = '\Visol\Newspermissions\Hooks\RecordListActionsHook';
